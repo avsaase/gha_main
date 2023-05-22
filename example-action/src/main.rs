d20b6195c3adf1d6ab82e56c774b3952f421f6a0
Alexander van Saase <avsaase@gmail.com>
2023-05-22 19:40:33 +0200

    Change action integration test

--- example-action/src/main.rs
+++ example-action/src/main.rs
@@ -1,16 +1,13 @@
 use std::env;
 extern crate gha_main;
 use gha_main::{gha_main, gha_result, GitHubActionResult};
-use ureq::serde_json;
 
 #[gha_main]
 fn main() -> GitHubActionResult {
     let args: Vec<String> = env::args().collect();
-    let url = &args[1];
+    let input = &args[1];
 
-    println!("Making GET request to {}", &url);
-    let response = ureq::get(url).call()?;
-    let json: serde_json::Value = response.into_json::<serde_json::Value>()?;
+    let parsed_u32 = input.parse::<u32>()?;
 
-    Ok(gha_result!(json))
+    Ok(gha_result!(parsed_u32))
 }
