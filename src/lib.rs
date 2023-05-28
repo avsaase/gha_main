974d67d96296c3a868b3a6711d87b1b43314d4e0
Alexander van Saase <avsaase@gmail.com>
2023-05-28 10:50:26 +0200

    Hide re-exports from docs

--- src/lib.rs
+++ src/lib.rs
@@ -54,8 +54,11 @@
 
 pub use gha_main_proc_macro::gha_main;
 
+#[doc(hidden)]
 pub extern crate anyhow;
+#[doc(hidden)]
 pub extern crate lazy_static;
+#[doc(hidden)]
 pub extern crate uuid;
 
 /// Return type for `main()`
